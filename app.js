9521fa92b0ffe69b346e2e9d268ea06894772eff
Taylor Chen <s68175@gmail.com>
2020-07-29 21:24:42 +0800

    Copy from todolist

---
+++ app.js
@@ -0,0 +1,26 @@
+// 載入 express 並建構應用程式伺服器
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+// 載入 method-override
+const methodOverride = require('method-override')
+
+// 引用路由器
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride('_method'))
+
+// 將 request 導入路由器
+app.use(routes)
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
